4dd197cbe9c96c155c184a6234e79b7b55c617f0
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-02 21:14:40 +0300

    HttpGet and HttpPost attributes added.

--- SocialContentWebSite/Controllers/CategoryController.cs
+++ SocialContentWebSite/Controllers/CategoryController.cs
@@ -21,6 +21,14 @@ namespace SocialContentWebSite.Controllers
             var categoryvalues = cm.GetAllBL();
             return View(categoryvalues);
         }
+        
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
         public ActionResult AddCategory(Category p)
         {
             cm.CategoryAddBL(p);
