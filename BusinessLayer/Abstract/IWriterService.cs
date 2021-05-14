1520e7955f2ea2804070152a390e8ee7ba609002
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-14 18:59:06 +0300

    Writer operations done.

---
+++ BusinessLayer/Abstract/IWriterService.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IWriterService
+    {
+        List<Writer> GetList();
+        void WriterAdd(Writer writer);
+        void WriterDelete(Writer writer);
+        void WriterUpdate(Writer writer);
+        Writer GetByID(int id);
+    }
+}
---
+++ SocialContentWebSite/Controllers/WriterController.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System.Web.Mvc;
+
+namespace SocialContentWebSite.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+
+        // GET: Writer
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
