b1be91e528c7e870b8acd4d35b26316043866fb1
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-19 19:53:42 +0300

    Heading operations are in progress.

---
+++ BusinessLayer/Abstract/IHeadingService.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+        void HeadingAdd(IHeadingService heading);
+        IHeadingService GetByID(int id);
+        void HeadingDelete(Heading heading);
+        void HeadingUpdate(Heading heading);
+    }
+}
---
+++ SocialContentWebSite/Controllers/HeadingController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System.Web.Mvc;
+
+namespace SocialContentWebSite.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
