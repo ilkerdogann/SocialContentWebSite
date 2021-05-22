60da4c8a195a3f85e6b432a55a904a7ccd097594
İlker Doğan <ilkerdogan1215@gmail.com>
2021-05-22 20:26:11 +0300

    Heading adding operations have been done.

--- SocialContentWebSite/Controllers/HeadingController.cs
+++ SocialContentWebSite/Controllers/HeadingController.cs
@@ -1,6 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace SocialContentWebSite.Controllers
@@ -9,6 +12,8 @@ namespace SocialContentWebSite.Controllers
     {
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
@@ -18,12 +23,20 @@ namespace SocialContentWebSite.Controllers
         [HttpGet]
         public ActionResult AddHeading()
         {
+            List<SelectListItem> valuecategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.CategoryName,
+                                                      Value = x.CategoryID.ToString()
+                                                  }).ToList();
+            ViewBag.vlc = valuecategory;
             return View();
         }
 
         [HttpPost]
         public ActionResult AddHeading(Heading p)
         {
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
